2848634fd6b2794715207ea17af002bda1080ee9
chrisgreen848 <45197319+chrisgreen848@users.noreply.github.com>
2018-11-20 10:58:38 +0000

    Update source.cpp

--- source.cpp
+++ source.cpp
@@ -1,7 +1,7 @@
 /******************************************************************************
 
-							  Online C++ Compiler.
-			   Code, Compile, Run and Debug C++ program online.
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
 Write your code in this editor and press "Run" button to compile and execute it.
 
 *******************************************************************************/
@@ -16,75 +16,61 @@ Write your code in this editor and press "Run" button to compile and execute it.
 using namespace std;
 
 class matrixManip {
+    private: // Class variables that can only be accessed by THIS class
 	wchar_t newMatrix[200000] = {};
 	char letterss[4] = { 'G','C','A','T' };
-	char gen[4] = {};
+	char gen[4];
 	int newMatrixSize = 0;
 	int counter;
 	string temp;
 	vector<string> combi;
 	int vectcounter = 0;
 	int iterator = 0;
+	vector<char> generatedMatrix;
+
 public:
 
 
-	void generateMatrix(int a) {
+	vector<char> generateMatrix(int a) {
 		srand(time(NULL));
 		unsigned long int generateNumber;
 		generateNumber = a;
 		int randomNumber;
-		char letters[4] = { 'G','C','A','T' };
+		char letters[4] = { 'G','C','A','T' }; //Stores useable characters
 		int count = 0;
-
-
+		
 		for (int x = 0; x < generateNumber; x++) {
-
 			count++;
 			randomNumber = rand() % 4;
 
+			if (randomNumber == 0){generatedMatrix.push_back ('G');}
 
-
-			if (randomNumber == 0)
-			{
-				newMatrix[x] = 'G';
-			}
-
-			if (randomNumber == 1) {
-				newMatrix[x] = 'C';
-			}
-			if (randomNumber == 2)
-			{
-				newMatrix[x] = 'A';
-			}
-			if (randomNumber == 3)
-			{
-				newMatrix[x] = 'T';
-			}
+			if (randomNumber == 1){generatedMatrix.push_back ('C');}
+				
+			if (randomNumber == 2){generatedMatrix.push_back ('A');}
+			
+			if (randomNumber == 3){generatedMatrix.push_back ('T');}
 		}
-
-
-		for (int x = 0; x < generateNumber + 1; x++) {
-
-			if (x == 0) {
-				cout << "[";
-			}
-			cout << " " << char(newMatrix[x]);
-
-			if (newMatrix[x] == NULL) {
-				cout << "]" << endl;
-				break;
-			}
-
-		}
+                    return generatedMatrix;
 	}
+void printGeneratedMatrix(){
+    for (std::vector<char>::iterator it = generatedMatrix.begin(); it != generatedMatrix.end(); ++it)
+		{
+             if (it == generatedMatrix.begin()) {
+				cout << "[ ";
+			}
+			cout << *it << " ";
+		}
+    	cout << "]" << endl;
+}
 
-	void generateCombinations() {
-
-
+	vector<string> generateCombinations(){
+		
+	
 		for (int position1 = 0; position1 < 4; position1++)
 		{
 
-			for (int position2 = 0; position2 < 4; position2++)
+			for (int position2 = 0; position2< 4; position2++)
 			{
 
 				for (int position3 = 0; position3 < 4; position3++)
@@ -97,20 +83,21 @@ public:
 						gen[1] = letterss[position2];
 						gen[2] = letterss[position3];
 						gen[3] = letterss[position4];
-						counter = 0;
+						counter= 0; // resets counter to 0 for function checker
 						functionChecker();
 						temp = temp + gen[0] + gen[1] + gen[2] + gen[3];
 						combi.push_back(temp);
-
+						
 						newMatrixSize = sizeof(newMatrix);
-						newMatrixSize = newMatrixSize / 4;
+						newMatrixSize = newMatrixSize/4;
 						vectcounter++;
-
-
+						
+						
 					}
 				}
 			}
 		}
+		return combi;
 	}
 
 	void printAllCombinations() {
@@ -118,30 +105,45 @@ public:
 		{
 
 			cout << *it << endl;
-
+			
 		}
 	}
 
-	void functionChecker() {
+	void functionChecker() {  // internal check
 		for (int randomMatrixChecker = 0; randomMatrixChecker < newMatrixSize; randomMatrixChecker++) {
 			if (newMatrix[randomMatrixChecker] == gen[0] && newMatrix[randomMatrixChecker + 1] == gen[1] && newMatrix[randomMatrixChecker + 2] == gen[2] && newMatrix[randomMatrixChecker + 3] == gen[3])
 			{
 				counter++;
 			}
 		}
+		cout <<"," << counter << endl;
 	}
+	
+/*	void functionChecker2(){
+	    
+	    int index=0;
+	    for (std::vector<char>::iterator it = generatedMatrix.begin(); it != generatedMatrix.end(); ++it)
+		{
+		    string temp = "";
+            temp = temp + generatedMatrix.at(it) + generatedMatrix(it+1) + generatedMatrix.at(it+2) + generatedMatrix.at(it+3;)
+            cout << temp;
+			cout << *it << endl;
+			
+		}
+	    
+	}*/
 
 	string nextCombination(string a) {
-
-
-
+		
+		
+		
 		for (std::vector<string>::iterator it = combi.begin(); it != combi.end(); ++it)
 		{
 
-
+			
 			if (a == *it) {
 				it++;
-				if (it == combi.end()) { temp = "AAAA"; return temp; }
+				if(it == combi.end()){temp = "AAAA"; return temp;} // if the last value of vectors entered. Starts again
 				temp = *it;
 				cout << "Next Combi is" << *it;
 				break;
@@ -153,21 +155,24 @@ public:
 
 int main() {
 
-	matrixManip one;
+	matrixManip one;  
 	cout << "Enter amount of letters you want to generate";
 	int number;
 	cin >> number;
-
-
-	one.generateMatrix(number);
-	one.generateCombinations();
+	vector <char> generatedMatrix;
+	vector <string> allCombinations;
+	
+	generatedMatrix = one.generateMatrix(number);
+	one.printGeneratedMatrix();
+	allCombinations=one.generateCombinations();
 	one.printAllCombinations();
-	cout << "Enter the string you want for next combination checker" << endl;
-	string combination = "";
-	cin >> combination;
-	combination = one.nextCombination(combination);
-	cout << "combination is" << combination;
+//one.functionChecker2();
+	//cout << "Enter the string you want for next combination checker" << endl;
+	//string combination = "";
+//	cin >> combination;
+	//combination = one.nextCombination(combination);
+	//cout << "combination is" << combination;
 	int end; // just so the cmd tool stays to show outputs - remove this from code before submitting
 	cin >> end;
-
+	
 }
